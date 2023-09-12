fbb59cc7ef78c3913f28ede6626003ef3deb7651
Felipe de Sousa <felipescoelho517@gmail.com>
2023-09-12 17:30:17 -0300

    add searches

---
+++ src/java/exercises/lists/searches/CatalogoLivros.java
@@ -0,0 +1,41 @@
+package java.exercises.lists.searches;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo,autor,anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getAutor().equalsIgnoreCase(autor)){
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos (int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getAnoPublicacao() <= anoFinal && l.getAnoPublicacao() >= anoInicial){
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+}
---
+++ src/java/exercises/lists/searches/SomaNumeros.java
@@ -0,0 +1,4 @@
+package java.exercises.lists.searches;
+
+public class SomaNumeros {
+}
