4b0c5405393415b0b306a02714b20c528e9954f5
Felipe de Sousa <felipescoelho517@gmail.com>
2023-09-29 18:40:04 -0300

    update convidados

---
+++ src/java/exercises/set/operations/ConjuntoConvidados.java
@@ -0,0 +1,47 @@
+package java.exercises.set.operations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void addConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removeConvidadoPorCodigoConvite(int codigoConvite){
+        if (!convidadoSet.isEmpty()){
+            convidadoSet.removeIf(c -> codigoConvite == c.getCodigoConvite());
+        }
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados");
+
+//        conjuntoConvidados.addConvidado("Convidado 1", 1234);
+//        conjuntoConvidados.addConvidado("Convidado 2", 1235);
+//        conjuntoConvidados.addConvidado("Convidado 3", 1235);
+//        conjuntoConvidados.addConvidado("Convidado 4", 1236);
+//
+//        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados");
+//
+//        conjuntoConvidados.removeConvidadoPorCodigoConvite(1234);
+//        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados");
+//
+//        conjuntoConvidados.exibirConvidados();
+    }
+}
---
+++ src/java/exercises/set/operations/Convidado.java
@@ -0,0 +1,33 @@
+package java.exercises.set.operations;
+
+import java.util.Objects;
+
+public class Convidado {
+    private String nome;
+    private int codigoConvite;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigoConvite());
+    }
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+}
